d55cc56ffa9cdd5e0f4e1bec00611bb0bd2cdd9c
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-09-23 16:14:57 +0300

    create Flutter App

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() async {
+  await ScreenUtil.ensureScreenSize();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Anime App',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          ),
+          home: const Scaffold(body: Center(child: Text('Hello, World!'))),
+        );
+      },
+    );
+  }
+}
